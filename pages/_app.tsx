6b7641c442c8fd3de8e36ac53189555c3e5366ad
Peter Zhang <ultraswordkirbypink@gmail.com>
2023-02-03 18:04:42 -0500

    started github section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,25 +7,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
   <>
     {/* inconsolata -- titles  */}
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@500&display=swap" rel="stylesheet"/>
-
     {/*  dancing script -- fancy  */}
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@500&display=swap" rel="stylesheet"></link>
-
     {/* indie flower -- descriptions */}
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@500&family=Indie+Flower&display=swap" rel="stylesheet"></link>
-
     {/* share tech mono -- general theme */}
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@500&family=Indie+Flower&family=Share+Tech+Mono&display=swap" rel="stylesheet"></link>
-    
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&family=Inconsolata:wght@500&family=Indie+Flower&family=Share+Tech+Mono&display=swap" rel="stylesheet"/>
+
     <Head>
       <link rel="icon" href="/favicon.ico" />
     </Head>
